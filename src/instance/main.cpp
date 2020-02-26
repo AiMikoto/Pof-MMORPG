b6530c1f7c0942d83abc1d96c90c350d20c0cdad
iarba <dociarba@gmail.com>
2020-02-26 09:19:55 +0200

    refactored main, exported port, added proper blocking

--- src/instance/main.cpp
+++ src/instance/main.cpp
@@ -1,10 +1,24 @@
 #include "instance/server.h"
 #include <boost/asio.hpp>
+#include <cstdlib>
+#include <iostream>
+#include <mutex>
 
-int main()
+int main(int argc, char **argv)
 {
   boost::asio::io_context ioc;
-  server s(ioc, 7777);
-  while(1){};
+  if(argc < 2)
+  {
+    std::cerr << "improper call of instance creation" << std::endl;
+    return 1;
+  }
+  // extract arguments
+  int port = std::atoi(argv[1]);
+  // create a server 
+  server s(ioc, port);
+  // block current thread
+  mutex m;
+  m.lock();
+  m.lock();
   return 0;
 }
