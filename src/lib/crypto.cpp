e6dd5e7ad26e03ee40ed67387766b1769d8ec3ee
iarba <dociarba@gmail.com>
2020-03-02 17:17:26 +0200

    split crypto into aes and rsa crypto

--- src/lib/crypto.cpp
+++ src/lib/crypto.cpp
@@ -1,9 +1,16 @@
 #include "lib/crypto.h"
 #include <openssl/evp.h>
+#include <openssl/rsa.h>
 #include <openssl/err.h>
 #include <iostream>
 
-std::string crypto::encrypt(std::string data)
+aes_crypto::aes_crypto(std::string key, std::string iv)
+{
+  this -> key = key;
+  this -> iv = iv;
+}
+
+std::string aes_crypto::encrypt(std::string data)
 {
   uint32_t size = data.length();
   // allocate buffer
@@ -22,7 +29,7 @@ std::string crypto::encrypt(std::string data)
   return std::string((char *)buffer, output_size);
 }
 
-std::string crypto::decrypt(std::string data)
+std::string aes_crypto::decrypt(std::string data)
 {
   uint32_t size = data.length();
   // allocate buffer
@@ -38,5 +45,47 @@ std::string crypto::decrypt(std::string data)
   output_size += len;
   // clean-up
   EVP_CIPHER_CTX_free(ctx);
+  return std::string((char *)buffer, output_size);
+}
+
+rsa_crypto::rsa_crypto(BIO *enc, BIO *dec)
+{
+  this -> enc = enc;
+  this -> dec = dec;
+}
+
+rsa_crypto::~rsa_crypto()
+{
+  BIO_free_all(enc);
+  BIO_free_all(dec);
+}
+
+std::string rsa_crypto::encrypt(std::string data)
+{
+  uint32_t size = data.length();
+  // initialise encryption operation
+  RSA *key = RSA_new();
+  PEM_read_bio_RSAPublicKey(enc, &key, NULL, NULL);
+  // allocate buffer
+  unsigned char buffer[RSA_size(key)];
+  // encrypt
+  uint32_t output_size = RSA_public_encrypt(size, (unsigned char *)data.c_str(), buffer, key, padding_convention);
+  // clean-up
+  RSA_free(key);
+  return std::string((char *)buffer, output_size);
+}
+
+std::string rsa_crypto::decrypt(std::string data)
+{
+  uint32_t size = data.length();
+  // initialise decryption operation
+  RSA *key = RSA_new();
+  PEM_read_bio_RSAPrivateKey(dec, &key, NULL, NULL);
+  // allocate buffer
+  unsigned char buffer[RSA_size(key)];
+  // decrypt
+  uint32_t output_size = RSA_private_decrypt(size, (unsigned char *)data.c_str(), buffer, key, padding_convention);
+  // clean-up
+  RSA_free(key);
   return std::string((char *)buffer, output_size);
 }
--- src/lib/crypto.h
+++ src/lib/crypto.h
@@ -1,17 +1,39 @@
-#ifndef LIB_CRYPTO_CPP
-#define LIB_CRYPTO_CPP
+#ifndef LIB_CRYPTO_H
+#define LIB_CRYPTO_H
 
 #include <string>
+#include <openssl/pem.h>
 
 class crypto
 {
 public:
-  std::string key;
-  std::string iv;
+  virtual std::string encrypt(std::string data) = 0;
+  virtual std::string decrypt(std::string data) = 0;
+};
+
+class aes_crypto:public crypto
+{
+public:
+  aes_crypto(std::string key, std::string iv);
   std::string encrypt(std::string data);
   std::string decrypt(std::string data);
 private:
   int block_size = 128;
+  std::string key;
+  std::string iv;
 };
 
-#endif // LIB_CRYPTO_CPP
+class rsa_crypto:public crypto
+{
+public:
+  rsa_crypto(BIO *enc, BIO *dec);
+  ~rsa_crypto();
+  std::string encrypt(std::string data);
+  std::string decrypt(std::string data);
+private:
+  BIO *enc;
+  BIO *dec;
+  int padding_convention = RSA_PKCS1_OAEP_PADDING;
+};
+
+#endif // LIB_CRYPTO_H
