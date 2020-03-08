63f2656817163699d51415c1405651bf7155602f
iarba <dociarba@gmail.com>
2020-03-08 06:30:30 +0000

    added filter to message applier

--- src/instance/misc.cpp
+++ src/instance/misc.cpp
@@ -1,9 +1,23 @@
 #include "instance/misc.h"
 #include "instance/client.h"
+#include "lib/chat.h"
 
 void send_message(user_card& uc, call c)
 {
-  client *cli = (client *)uc.aux;
-  cli -> safe_write(c);
+  chat_target target = static_cast<chat_target>(c.tree().get<int>("target"));
+  bool approve = false;
+  if(target == local)
+  {
+    approve = true;
+  }
+  if(target == world)
+  {
+    approve = true;
+  }
+  if(approve)
+  {
+    client *cli = (client *)uc.aux;
+    cli -> safe_write(c);
+  }
 }
 
