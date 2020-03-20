a918247afc4b44314a9f6350bebb82dcf58da9b5
iarba <dociarba@gmail.com>
2020-03-20 08:02:30 +0000

    added collider component, deleted container

--- src/components/collider.cpp
+++ src/components/collider.cpp
@@ -1,55 +1,27 @@
-#include "phys/container.h"
+#include "components/collider.h"
 #include <algorithm>
 
-container::container(graphics::Mesh *o, container_t c_type, bool movable, bool collidable, bool collides)
+collider::collider(glm::dvec3 size, collider_t c_type) : Component(true)
 {
-  this -> o = o;
-  this -> type = c_type;
-  this -> movable = movable;
-  this -> collidable = collidable;
-  this -> collides = collides;
-  this -> velocity = {0, 0, 0};
-  this -> force_acc = {0, 0, 0};
-  m = 1;
-  if(collidable)
-  {
-    im = 0;
-  }
-  if(collides)
-  {
-    im = 1;
-  }
-  // if the object neither collides nor is collidable, im is never used
+  this -> c_type = c_type;
+  this -> size = size;
+  setType();
 }
 
-void container::add_force(glm::dvec3 force)
+collider::~collider()
 {
-  this -> force_acc += force;
 }
 
-void container::tick(double delta)
-{
-  velocity += (1 / m) * force_acc * delta;
-  force_acc = {0, 0, 0};
-  o -> transform.position += velocity * delta;
-}
-
-container::~container()
-{
-  delete o;
-}
-
-aabb container::to_aabb()
+aabb collider::to_aabb()
 {
   aabb ret;
-  glm::dvec3 position = this -> o -> transform.position;
-  double oneeighty = 180;
-  glm::dvec3 scale = this -> o -> transform.scale;
-  glm::dvec3 size = this -> o -> meshScale;
-  if(this -> type == sphere)
+  glm::dvec3 position = this -> gameObject -> transform.position;
+  glm::dvec3 scale = this -> gameObject -> transform.scale;
+  glm::dvec3 size = this ->size;
+  if(this -> c_type == sphere)
   {
     // Assumptions
-    // scale - x = y = z = radius
+    // size  - x = y = z = radius
     // rotation - around any axis -> no impact on aabb	
     ret.minx = position.x - scale.x * size.x / 2;
     ret.maxx = position.x + scale.x * size.x / 2;
@@ -58,10 +30,10 @@ aabb container::to_aabb()
     ret.minz = position.z - scale.z * size.z / 2;
     ret.maxz = position.z + scale.z * size.z / 2;
   }
-  if(this -> type == caps)
+  if(this -> c_type == caps)
   {
     // Assumptions
-    // scale - x = z = radius, y = height
+    // size - x = z = radius, y = height
     // rotation - only around y axis -> no impact on aabb
     ret.minx = position.x - scale.x * size.x / 2;
     ret.maxx = position.x + scale.x * size.x / 2;
@@ -70,7 +42,7 @@ aabb container::to_aabb()
     ret.minz = position.z - scale.z * size.z / 2;
     ret.maxz = position.z + scale.z * size.z / 2;
   }
-  if(this -> type == box)
+  if(this -> c_type == box)
   {
     // Assumptions
     glm::dvec4 points[8];
@@ -95,9 +67,14 @@ aabb container::to_aabb()
   return ret;
 }
 
-void get_points(container *c, glm::dvec4 *points)
+void collider::setType()
 {
-  glm::dvec3 size = c -> o -> meshScale;
+  type = typeid(*this).name();
+}
+
+void get_points(collider *c, glm::dvec4 *points)
+{
+  glm::dvec3 size = c -> size;
   points[0] = glm::dvec4( size.x / 2,  size.y / 2,  size.z / 2, 1);
   points[1] = glm::dvec4( size.x / 2,  size.y / 2, -size.z / 2, 1);
   points[2] = glm::dvec4( size.x / 2, -size.y / 2,  size.z / 2, 1);
@@ -106,7 +83,7 @@ void get_points(container *c, glm::dvec4 *points)
   points[5] = glm::dvec4(-size.x / 2,  size.y / 2, -size.z / 2, 1);
   points[6] = glm::dvec4(-size.x / 2, -size.y / 2,  size.z / 2, 1);
   points[7] = glm::dvec4(-size.x / 2, -size.y / 2, -size.z / 2, 1);
-  glm::mat4x4 m = c -> o -> transform.model();
+  glm::mat4x4 m = c -> gameObject -> transform.model();
   for(int i = 0; i < 8; i++)
   {
     points[i] = m * points[i];
