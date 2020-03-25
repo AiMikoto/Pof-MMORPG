b9e1bcc5ef4e6523e313006b55f070aa804022d8
iarba <dociarba@gmail.com>
2020-03-25 08:33:30 +0000

    added scene and slicing skeleton to instance

--- src/instance/game.cpp
+++ src/instance/game.cpp
@@ -1,9 +1,12 @@
 #include "instance/game.h"
 #include "instance/client.h"
 #include "lib/log.h"
+#include "include/common_macro.h"
 
 user_card_library ucl;
 user_card_library uclp;
+
+engine::Scene *current;
 
 bool loaded = false;
 
@@ -14,12 +17,23 @@ void ucl_destroy()
 void unload()
 {
   BOOST_LOG_TRIVIAL(trace) << "unloading instance";
-  loaded = false;
+  if(is_loaded())
+  {
+    loaded = false;
+    // TODO: wait for slicing thread to finish current slice
+    BOOST_LOG_TRIVIAL(trace) << "deleting scene";
+    // TODO: save scene
+    delete current;
+  }
 }
 
 void load()
 {
+  unload();
   BOOST_LOG_TRIVIAL(trace) << "loading instanced";
+  // TODO: load scene
+  current = new engine::Scene();
+  // TODO: create slicing thread
   loaded = true;
 }
 
@@ -27,3 +41,15 @@ bool is_loaded()
 {
   return loaded;
 }
+
+void slicer()
+{
+  // TODO: update last timestamp
+  forever
+  {
+    // TODO: sleep until next tick
+    // TODO: update last timestamp
+    tick(current);
+  }
+}
+
--- src/instance/game.h
+++ src/instance/game.h
@@ -2,16 +2,22 @@
 #define INSTANCE_GAME_H
 
 #include "common/user_card.h"
+#include "scene/scene.h"
+#include "phys/slicing.h"
 
 extern user_card_library ucl;
 extern user_card_library uclp;
 
 void ucl_destroy();
 
+extern engine::Scene *current;
+
 void unload();
 
 void load();
 
 bool is_loaded();
+
+void slicer();
 
 #endif // INSTANCE_GAME_H
