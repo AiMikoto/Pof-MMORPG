c52be97e45c4d927daa50bc4610dce30779f68b2
iarba <dociarba@gmail.com>
2020-04-06 12:16:33 +0100

    refactored manipulator main

--- src/manipulator/main.cpp
+++ src/manipulator/main.cpp
@@ -5,45 +5,59 @@
 #include "include/common_macro.h"
 #include "lib/log.h"
 
+manipulator *man;
+
+bool try_ss(std::string line)
+{
+  if(line == "set slicer on")
+  {
+    man -> set_slicer(true);
+    return true;
+  }
+  if(line == "set slicer off")
+  {
+    man -> set_slicer(false);
+    return true;
+  }
+  return false;
+}
+
+bool try_sps(std::string line)
+{
+  double sps;
+  if(sscanf(line.c_str(), "set sps %lf", &sps) == 1)
+  {
+    man -> set_sps(sps);
+    return true;
+  }
+  return false;
+}
+
+bool try_move(std::string line)
+{
+  unsigned long long id;
+  glm::dvec3 pos;
+  if(sscanf(line.c_str(), "move %llu {%lf, %lf, %lf}", &id, &pos.x, &pos.y, &pos.z) == 4)
+  {
+    man -> obj_move(id, pos);
+    return true;
+  }
+  return false;
+}
+
 int main()
 {
   log_init("manipulator");
-  log_mute(); // we want silence for reading purposes
   init_crypto("keys/public_key.pem");
-  manipulator man("localhost", 7000, "fish");
+  man = new manipulator("localhost", 7000, "fish");
+  log_mute(); // we want silence for reading purposes
   forever
   {
     std::string line;
     getline(std::cin, line);
-    if(line == "set slicer off")
-    {
-      man.set_slicer(false);
-      continue;
-    }
-    if(line == "set slicer on")
-    {
-      man.set_slicer(true);
-      continue;
-    }
-    {
-      double sps;
-      if(sscanf(line.c_str(), "set sps %lf", &sps) == 1)
-      {
-        man.set_sps(sps);
-        continue;
-      }
-    }
-    {
-      unsigned long long id;
-      glm::dvec3 pos;
-      if(sscanf(line.c_str(), "move %llu {%lf, %lf, %lf}", &id, &pos.x, &pos.y, &pos.z) == 4)
-      {
-        man.obj_move(id, pos);
-        continue;
-      }
-    }
-    printf("unknown command\n");
+    try_ss(line) || try_sps(line) || try_move(line) || printf("unknown command\n");
   }
   return 0;
   destroy_crypto();
+  delete man;
 }
