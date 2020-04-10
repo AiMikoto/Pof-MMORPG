0ebb1b8b7396c8de43bd87e59612565c1b37c722
iarba <dociarba@gmail.com>
2020-04-10 15:03:13 +0100

    improved scene viewer and extra debugging

--- src/instance/game.cpp
+++ src/instance/game.cpp
@@ -67,12 +67,13 @@ bool is_loaded()
 
 unsigned long long game_inject_object()
 {
-  engine::GameObject *aux = new engine::GameObject();
   engine::GameObject *go = new engine::GameObject();
+  BOOST_LOG_TRIVIAL(trace) << "creating new game object";
   slicer_acquire();
-  unsigned long long pos = current -> addGameObject(aux);
+  unsigned long long pos = current -> addGameObject(go);
   slicer_inject_object(pos, go);
   slicer_release();
+  BOOST_LOG_TRIVIAL(trace) << "created new game object";
   return pos;
 }
 
